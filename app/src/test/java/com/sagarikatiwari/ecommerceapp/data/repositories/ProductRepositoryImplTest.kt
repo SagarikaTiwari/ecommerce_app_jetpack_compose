da0c6d448dfb0446c50ea199614d8669b36954ca
sagarikatiwari <sagtiwar@publicisgroupe.net>
2022-12-15 16:09:09 +0530

    Added Unit Test To ProductRepositoryImplTest.kt

--- app/src/test/java/com/sagarikatiwari/ecommerceapp/data/repositories/ProductRepositoryImplTest.kt
+++ app/src/test/java/com/sagarikatiwari/ecommerceapp/data/repositories/ProductRepositoryImplTest.kt
@@ -1,8 +1,11 @@
 package com.sagarikatiwari.ecommerceapp.common.data.remote.api
 
+import com.sagarikatiwari.ecommerceapp.data.entities.ProductDetailsEntity
 import com.sagarikatiwari.ecommerceapp.data.entities.ProductEntity
 import com.sagarikatiwari.ecommerceapp.data.repositories.ProductRepositoryImpl
 import com.sagarikatiwari.ecommerceapp.data.remote.ProductService
+import com.sagarikatiwari.ecommerceapp.domain.entities.Product
+import com.sagarikatiwari.ecommerceapp.domain.entities.ProductDetails
 import com.sagarikatiwari.ecommerceapp.domain.mapper.ProductDetailsEntityDataMapper
 import com.sagarikatiwari.ecommerceapp.domain.mapper.ProductEntityDataMapper
 import io.mockk.coEvery
@@ -22,27 +25,122 @@ class ProductRepositoryImplTest {
 
     @Before
     fun setup() {
-        repository = ProductRepositoryImpl(service, productEntityToProductDataMapper, productDetailsEntityDataMapper)
+        repository = ProductRepositoryImpl(
+            service,
+            productEntityToProductDataMapper,
+            productDetailsEntityDataMapper
+        )
     }
 
     @Test
-    fun `when get product List is  called then it returns entity into Business Objects`() = runTest {
-        coEvery {
-            service.getProductList()
-        } returns (0..2).map {
-            ProductEntity(
-                "id",
-                "title",
-                "description $it",
-                6.0,
-                ""
-            )
+    fun `when get product List is  called then it returns entity into Business Objects`() =
+        runTest {
+
+            val listOfProductEntity = (0..2).map {
+                ProductEntity(
+                    "id",
+                    "title",
+                    "description $it",
+                    6.0,
+                    ""
+                )
+            }
+            val listOfProducts = (0..2).map {
+                Product(
+                    "title",
+                    "description $it",
+                    6.0,
+                    "",
+                    "id"
+                )
+            }
+            coEvery {
+                service.getProductList()
+            } returns listOfProductEntity
+
+            coEvery {
+                productEntityToProductDataMapper.mapProdcutEntityToProduct(
+                    ProductEntity(
+                        id = "id",
+                        title = "title",
+                        description = "description 0",
+                        price = 6.0,
+                        imageUrl = ""
+                    )
+                )
+            } returns listOfProducts.get(0)
+            coEvery {
+                productEntityToProductDataMapper.mapProdcutEntityToProduct(
+                    ProductEntity(
+                        id = "id",
+                        title = "title",
+                        description = "description 1",
+                        price = 6.0,
+                        imageUrl = ""
+                    )
+                )
+            } returns listOfProducts.get(1)
+            coEvery {
+                productEntityToProductDataMapper.mapProdcutEntityToProduct(
+                    ProductEntity(
+                        id = "id",
+                        title = "title",
+                        description = "description 2",
+                        price = 6.0,
+                        imageUrl = ""
+                    )
+                )
+            } returns listOfProducts.get(2)
+
+            val products = repository.getProductList().data!!
+
+            assert(products.size == 3)
+            if (products != null) {
+                assert(products.get(1).description == "description 1")
+            }
         }
-        val products = repository.getProductList().data!!
-
-        assert(products.size == 3)
-        assert(products[1].description == "description 1")
-    }
 
 
+    @Test
+    fun `when get Product Detail is called then it returns entity into business object`() =
+        runTest {
+            coEvery {
+                service.getProductDetails(any())
+            } returns ProductDetailsEntity(
+                "title",
+                "desc",
+                "full_desc",
+                10.0,
+                "",
+                listOf("pros"),
+                listOf("cons")
+            )
+
+            coEvery {
+                productDetailsEntityDataMapper.mapProductDetailsEntityToProductDetails(
+                    ProductDetailsEntity(
+                        "title",
+                        "desc",
+                        "full_desc",
+                        10.0,
+                        "",
+                        listOf("pros"),
+                        listOf("cons")
+                    )
+                )
+            } returns ProductDetails(
+                "title",
+                "desc",
+                "full_desc",
+                "10.0",
+                "",
+                listOf("pros"),
+                listOf("cons")
+            )
+
+            assert(repository.getProductDetails("1").data?.title == "title")
+            assert(repository.getProductDetails("13").data?.description == "desc")
+            assert(repository.getProductDetails("23").data?.price == "10.0")
+
+        }
 }
